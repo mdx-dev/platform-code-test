de35c1261bca18345d8225a0bab2d936607a8dd5
Mike Carmody <mike.e.carmody@gmail.com>
2015-02-21 17:50:49 -0600

    Test complete

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,28 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
+    next if award.name == 'Blue Distinction Plus'
+    case award.name
+    when 'Blue First'
       award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+      award.quality += 1 if award.quality < 50
+      award.quality += 1 if award.quality < 50 && award.expires_in < 0
+    when 'Blue Compare'
+      award.quality += 1 if award.quality < 50
+      award.quality += 1 if award.expires_in < 11 && award.quality < 50
+      award.quality += 1 if award.expires_in < 6 && award.quality < 50
+      award.expires_in -= 1
+      award.quality = 0 if award.expires_in < 0
+    when 'Blue Star'
+      award.quality -= 2 if award.quality > 0
+      award.expires_in -= 1
+      award.quality -= 2 if award.expires_in < 0
+      award.quality = 0 if award.quality < 0
+    else
+      # handle normal awards
+      award.quality -= 1 if award.quality > 0
+      award.expires_in -= 1
+      award.quality -= 1 if award.expires_in < 0 && award.quality > 0
     end
   end
 end
