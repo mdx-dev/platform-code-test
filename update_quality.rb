2d2659f3b96a562e309f58009b818c9d4b44963c
Ed Posnak <ed.posnak@gmail.com>
2015-02-27 13:48:12 -0800

    get failing specs passing convert to case statements, which will be useful in the refactor phase add comments from business rules separate side-effect of decrementing expires_in

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,56 @@
 require 'award'
 
+# Updates quality for all given awards. Also reduces expires_in by 1 for all awards.
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+
+    # "Blue Distinction Plus", being a highly sought distinction, never decreases in quality (and never has its expiration reduced)
+    next if award.name == 'Blue Distinction Plus'
+
+    # update quality based on the present value of expires_in
+    award.quality = case award.name
+                        when 'Blue First'
+                          # "Blue First" awards actually increase in quality the older they get
+                          quality_increase = 1
+                          # Once the expiration date has passed, quality score degrades twice as fast
+                          # Apparently they increase twice as fast as well for Blue First
+                          quality_increase *= 2 if award.expires_in <= 0
+                          [award.quality + quality_increase, 50].min
+
+                        when 'Blue Compare'
+                          # "Blue Compare", similar to "Blue First", increases in quality as the expiration date approaches;
+                          # Quality increases by 2 when there are 10 days or less left, and by 3 where there are 5 days or less left,
+                          # but quality value drops to 0 after the expiration date.
+                          if award.expires_in > 0
+                            quality_increase = case award.expires_in
+                                               when 1..5
+                                                 3
+                                               when 5..10
+                                                 2
+                                               else
+                                                 1
+                                               end
+                            [award.quality + quality_increase, 50].min
+                          else
+                            0
+                          end
+
+                        else
+                          # normal awards decrease in quality (implied requirement)
+                          quality_decrease = 1
+
+                          # Once the expiration date has passed, quality score degrades twice as fast
+                          quality_decrease *= 2 if award.expires_in <= 0
+
+                          # "Blue Star" awards should lose quality value twice as fast as normal awards.
+                          quality_decrease *= 2 if award.name == 'Blue Star'
+                          [award.quality - quality_decrease, 0].max
+                        end
+
+
+
+    # 3. decrease expires_in
+    award.expires_in -= 1
+
   end
 end
