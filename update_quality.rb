6b3358d8b6638cfcf1968a45d2e18ec658acf68f
Matt Harting <matt@psslabs.com>
2015-04-24 17:49:33 -0500

    update quality calcs to pass tests

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,57 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
+    award.expires_in -= 1
     if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
+      expired = true
+    else
+      expired = false
+    end
+    case award.name
+    when 'Blue First'
+      award.quality += 1
+      if expired
+        award.quality += 1
+      end
+      if award.quality > 50
+        award.quality = 50
+      end
+    when 'Blue Distinction Plus'
+      award.quality = 80
+# don't change expire date
+      award.expires_in += 1
+    when 'Blue Compare'
+      if expired
+        award.quality = 0
       else
-        if award.quality < 50
+        case award.expires_in + 1
+        when (6..10)
+          award.quality += 2
+        when (0..5)
+          award.quality += 3
+        else
           award.quality += 1
         end
       end
+      if award.quality > 50
+        award.quality = 50
+      end
+    when 'Blue Star'
+      if expired
+        award.quality -= 4
+      else
+        award.quality -= 2
+      end
+    else
+# default case for normal awards
+      if expired
+        award.quality -= 2
+      else
+        award.quality -= 1
+      end
+    end
+    if award.quality < 0
+      award.quality = 0
     end
   end
 end
