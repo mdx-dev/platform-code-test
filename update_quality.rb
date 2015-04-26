757cf4a3fed1db28300204e0318e8d140d02d33b
Matt Harting <matt@psslabs.com>
2015-04-26 17:47:00 -0500

    change update_quality to invoke class method

--- update_quality.rb
+++ update_quality.rb
@@ -2,57 +2,6 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    award.expires_in -= 1
-    if award.expires_in < 0
-      expired = true
-    else
-      expired = false
-    end
-    max_quality = 50
-    case award.name
-    when 'Blue Compare'
-      if expired
-        award.quality = 0
-      else
-        case award.expires_in
-        when (6..10)
-          award.quality += 2
-        when (0..5)
-          award.quality += 3
-        else
-          award.quality += 1
-        end
-      end
-    when 'Blue Distinction Plus'
-      award.quality = 80
-      max_quality = 80
-# don't change expire date
-      award.expires_in += 1
-    when 'Blue First'
-      if expired
-        award.quality += 2
-      else
-        award.quality += 1
-      end
-    when 'Blue Star'
-      if expired
-        award.quality -= 4
-      else
-        award.quality -= 2
-      end
-    else
-# default case for normal awards
-      if expired
-        award.quality -= 2
-      else
-        award.quality -= 1
-      end
-    end
-    if award.quality > max_quality
-      award.quality = max_quality
-    end
-    if award.quality < 0
-      award.quality = 0
-    end
+    award.update
   end
 end
