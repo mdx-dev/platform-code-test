a52a0633f88ca84922e7cc652890ae604a98a329
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-05 16:24:27 -0400

    Refactored if-statement - Blue Compare pass

--- update_quality.rb
+++ update_quality.rb
@@ -3,20 +3,35 @@ require 'award'
 def update_quality(awards)
   awards.each do |award|
     
+    # NORMAL ITEM    
     if award.name == 'NORMAL ITEM'
       award.expires_in -= 1
       return if award.quality == 0
+      
       award.quality -= 1
       award.quality -= 1 if award.expires_in <= 0
     end
 
+    # BLUE FIRST
     if award.name == 'Blue First'
       award.expires_in -= 1
       return if award.quality == 50
+      
       award.quality += 1
-      return if award.quality == 50
-      award.quality += 1 if award.expires_in <= 0
+      award.quality += 1 if award.expires_in <= 0 && award.quality < 50
     end
+
+    # BLUE COMPARE
+    if award.name == 'Blue Compare'
+      award.expires_in -= 1
+      return if award.quality >= 50
+      return award.quality = 0 if award.expires_in < 0
+
+      award.quality += 1
+      award.quality += 1 if award.expires_in < 10 && award.quality < 50
+      award.quality += 1 if award.expires_in < 5 && award.quality < 50
+    end
+
 
 
     # if award.name != 'Blue First' && award.name != 'Blue Compare'
