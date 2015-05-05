bba275b6453a19ad28f44d4d6127d29fe8c390a5
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-05 16:29:51 -0400

    Refactored if-statement - all pass!

--- update_quality.rb
+++ update_quality.rb
@@ -32,52 +32,14 @@ def update_quality(awards)
       award.quality += 1 if award.expires_in < 5 && award.quality < 50
     end
 
+    # BLUE STAR
+    if award.name == 'Blue Star'
+      award.expires_in -= 1
+      return if award.quality == 0
 
+      award.quality -= 2
+      award.quality -= 2 if award.expires_in <= 0
+    end
 
-    # if award.name != 'Blue First' && award.name != 'Blue Compare'
-    #   if award.quality > 0
-    #     if award.name == 'Blue Star'
-    #       award.quality -= 2
-    #     elsif award.name != 'Blue Distinction Plus'
-    #       award.quality -= 1
-    #     end
-    #   end
-    # elsif award.quality < 50
-    #   award.quality += 1
-    #   if award.name == 'Blue Compare'
-    #     if award.expires_in < 11
-    #       if award.quality < 50
-    #         award.quality += 1
-    #       end
-    #     end
-    #     if award.expires_in < 6
-    #       if award.quality < 50
-    #         award.quality += 1
-    #       end
-    #     end
-    #   end
-    # end
-    # if award.name != 'Blue Distinction Plus'
-    #   award.expires_in -= 1
-    # end
-    # if award.expires_in < 0
-    #   if award.name != 'Blue First'
-    #     if award.name != 'Blue Compare'
-    #       if award.quality > 0
-    #         if award.name != 'Blue Distinction Plus'
-    #           if award.name == 'Blue Star'
-    #             award.quality -= 2
-    #           elsif
-    #             award.quality -= 1
-    #           end
-    #         end
-    #       end
-    #     else
-    #       award.quality = award.quality - award.quality
-    #     end
-    #   elsif award.quality < 50
-    #     award.quality += 1
-    #   end
-    # end
   end
 end
