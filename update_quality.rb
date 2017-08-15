cca245c58a08a9e71127f42648b3b1933b36ad7b
projectmaking123 <jason.89.wang@gmail.com>
2017-08-15 16:01:26 -0400

    changed award.rb to award class with initialize method

--- update_quality.rb
+++ update_quality.rb
@@ -1,33 +1,41 @@
-require 'award'
+require_relative 'award'
 
 def update_quality(awards)
+
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
+
+    if award.name != 'Blue First' && award.name != 'Blue Compare' && award.name != 'Blue Star'
+      if award.name != 'Blue Distinction Plus' && award.quality > 0
+        award.quality -= 1
       end
     else
       if award.quality < 50
         award.quality += 1
+
         if award.name == 'Blue Compare'
+
           if award.expires_in < 11
             if award.quality < 50
               award.quality += 1
             end
           end
+
           if award.expires_in < 6
             if award.quality < 50
               award.quality += 1
             end
           end
+
         end
+
       end
+      
     end
+
     if award.name != 'Blue Distinction Plus'
       award.expires_in -= 1
     end
+
     if award.expires_in < 0
       if award.name != 'Blue First'
         if award.name != 'Blue Compare'
@@ -45,5 +53,15 @@ def update_quality(awards)
         end
       end
     end
+
+
+
   end
+
 end
+
+award = Award.new("Blue First", 5, 10)
+p award
+p award.quality
+update_quality([award])
+p award.quality
