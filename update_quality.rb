387a9b05edd1f26cab8b1140141da6bc1a322412
projectmaking123 <jason.89.wang@gmail.com>
2017-08-15 22:21:59 -0400

    got all test to pass, will refactor more

--- update_quality.rb
+++ update_quality.rb
@@ -1,51 +1,69 @@
 require_relative 'award'
 
+def blue_star_update(award)
+  if award.expires_in > 1 && award.expires_in < 6
+    award.quality -= 1
+  end
+  if award.expires_in == 1
+    award.quality -= 3
+  end
+end
+
+def norm_update(award)
+  if award.quality > 0
+    return award.quality -= 1
+  end
+end
+
+def blue_distinct_update(award)
+  if award.name == 'Blue Distinction Plus'
+    return award.quality
+  end
+end
+
+def blue_compare_update(award)
+  if award.expires_in < 11 && award.quality < 50
+    award.quality += 1
+  end
+  if award.expires_in < 6 && award.quality < 50
+    award.quality += 1
+  end
+end
+
 def update_quality(awards)
-
   awards.each do |award|
+    return if blue_distinct_update(award)
 
-    if award.name != 'Blue First' && award.name != 'Blue Compare' && award.name != 'Blue Star'
-      if award.name != 'Blue Distinction Plus' && award.quality > 0
-        award.quality -= 1
-      end
+    if award.name != 'Blue First' && award.name != 'Blue Compare'
+      norm_update(award)
     else
       if award.quality < 50
         award.quality += 1
-
         if award.name == 'Blue Compare'
-
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-
+          blue_compare_update(award)
         end
-
       end
-      
     end
 
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
+    award.expires_in -= 1
+
+    if award.expires_in > 0 && award.quality > 0
+      if award.name == 'Blue Star'
+        award.quality -= 1
+      end
     end
 
     if award.expires_in < 0
       if award.name != 'Blue First'
         if award.name != 'Blue Compare'
           if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
+            award.quality -= 1
+            if award.name == 'Blue Star'
+              award.quality -= 2
             end
           end
         else
-          award.quality = award.quality - award.quality
+          award.quality = 0
         end
       else
         if award.quality < 50
@@ -53,15 +71,5 @@ def update_quality(awards)
         end
       end
     end
-
-
-
   end
-
 end
-
-award = Award.new("Blue First", 5, 10)
-p award
-p award.quality
-update_quality([award])
-p award.quality
--- update_quality_spec.rb
+++ update_quality_spec.rb
@@ -177,7 +177,7 @@ describe '#update_quality' do
       end
 
       context 'given a Blue Star award' do
-        before { pending }
+        # before { pending }
         let(:name) { 'Blue Star' }
         before { award.expires_in.should == initial_expires_in-1 }
 
