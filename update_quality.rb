a0e32b1453be0b980a06e3a8e1a4b638270a74e3
cohenaj194 <cohenaj194@gmail.com>
2020-01-11 12:46:57 -0800

    sanitize update_quality into functions and make rubocop happy

--- update_quality.rb
+++ update_quality.rb
@@ -2,38 +2,99 @@
 
 require_relative 'award'
 
+# "Blue Distinction Plus" is static at quality of 80 and never changes
+def static_blue_distinction_plus_quality(award)
+  award.quality = 80
+end
+
+# 'Blue First' increases with age
+def age_blue_first_quality(award)
+  # increase 2 if expired
+  if award.expires_in <= 0
+    award.quality += 2
+  # increase 1 if valid
+  else
+    award.quality += 1
+  end
+end
+
+# normal awards decrease with age
+def age_normal_quality(award)
+  # reduce 2 if expired
+  if award.expires_in <= 0
+    award.quality -= 2
+  # reduce 1 if valid
+  else
+    award.quality -= 1
+  end
+end
+
+# 'Blue Star' same as normal but degrades twice as fast
+def age_blue_star_quality(award)
+  # reduce 4 if expired
+  if award.expires_in <= 0
+    award.quality -= 4
+  # reduce 2 if valid
+  else
+    award.quality -= 2
+  end
+end
+
+# 'Blue Compare' increases with age
+# then drops to 0 once expired
+def age_blue_compare_quality(award)
+  # quality to 0 if expired
+  if award.expires_in <= 0
+    award.quality = 0
+  # quality +3 if 5 days to expiring
+  elsif award.expires_in <= 5
+    award.quality += 3
+  # quality +2 if 10 days to expiring
+  elsif award.expires_in <= 10
+    award.quality += 2
+  # quality +1 if more than 10 days to expiring
+  else
+    award.quality += 1
+  end
+end
+
+def check_quality_limits(award)
+  # 'Blue Distinction Plus' is static
+  return if award.name == 'Blue Distinction Plus'
+
+  # make sure 0 <= quality <= 50
+  if award.quality.negative?
+    award.quality = 0
+  elsif award.quality > 50
+    award.quality = 50
+  end
+end
+
+def update_age(award)
+  # 'Blue Distinction Plus' is static
+  return if award.name == 'Blue Distinction Plus'
+
+  # else reduce exparation date
+  award.expires_in -= 1
+end
+
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        award.quality -= 1 if award.name != 'Blue Distinction Plus'
-      end
+    case award.name
+    when 'Blue Distinction Plus'
+      static_blue_distinction_plus_quality(award)
+    when 'Blue First'
+      age_blue_first_quality(award)
+    when 'Blue Star'
+      age_blue_star_quality(award)
+    when 'Blue Compare'
+      age_blue_compare_quality(award)
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            award.quality += 1 if award.quality < 50
-          end
-          if award.expires_in < 6
-            award.quality += 1 if award.quality < 50
-          end
-        end
-      end
+      age_normal_quality(award)
     end
-    award.expires_in -= 1 if award.name != 'Blue Distinction Plus'
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            award.quality -= 1 if award.name != 'Blue Distinction Plus'
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        award.quality += 1 if award.quality < 50
-      end
-    end
+    # make sure quality limits are enforced
+    check_quality_limits(award)
+    # age the award
+    update_age(award)
   end
 end
