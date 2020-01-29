2a864e709af62df637ea63b463e769d682c777db
fifiten82726 <fifiteen82726@gmail.com>
2020-01-28 21:51:08 -0500

    Refactor: create 3 classes, blue compare, dist plus, first

---
+++ model/blue_compare.rb
@@ -0,0 +1,2 @@
+class BlueCompare
+end
---
+++ model/blue_first.rb
@@ -0,0 +1,7 @@
+class BlueFirst
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
+  end
+end
