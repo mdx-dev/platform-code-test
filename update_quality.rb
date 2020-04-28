e801cf90edb9b1f6ae7829478e96e06e89270685
er-san <smokecode@pm.me>
2020-04-28 13:17:12 -0400

    refactor update_quality method

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,19 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
+    case award.name
+    when 'Blue First'
+      award.update_blue_first_quality
+    when 'Blue Compare'
+      award.update_blue_compare_quality
+    when 'Blue Star'
+      award.update_blue_star_quality
+    when 'Blue Distinction Plus'
+      award.quality = Award.blue_distinction_plus_quality
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+      award.update_quality
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+    #excluded blue distinction for this because tests did not want it to be decremented
+    award.expires_in -= 1 if !award.is_blue_distinction_plus?
   end
 end
