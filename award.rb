d6e25f5136d0baa06a61cde5da2cea22808af8ed
Sandy Edwards <sedwardscode@gmail.com>
2020-06-19 13:02:36 -0400

    Adds Blue Star logic and refactors Award to class
    - Implements logic for award with 'Blue Star' name which decreases in quality twice as fast as normal awards
    
    - Refactors Award to ruby class, adding necessary methods for handling different update scenarios

--- award.rb
+++ award.rb
@@ -1,1 +1,36 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+  attr_accessor :name, :expires_in, :quality
+
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
+  end
+
+  def active?
+    @expires_in >= 0
+  end
+
+  def decrement_quality
+    return if @quality <= 0
+    @name == 'Blue Star' ? @quality -= 2 : @quality -= 1
+  end
+
+  def increment_quality
+    if @quality < 50
+      @quality += 1
+      increment_active_blue_compare
+    end
+  end
+
+  def increment_active_blue_compare
+    return if @name != 'Blue Compare' || !active?
+    @quality += 1 if @expires_in < 11
+    @quality += 1 if @expires_in < 6
+  end
+
+  def handle_expired
+    return @quality = 0 if @name == 'Blue Compare'
+    @name != 'Blue First' ? decrement_quality : increment_quality
+  end
+end
