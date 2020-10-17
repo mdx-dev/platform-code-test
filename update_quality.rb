faec50562b49b8df4fb46e391ab8f88e765bc0b5
zainsattar18 <zainsattar18@gmail.com>
2020-10-17 09:10:08 -0400

    all tests passing

--- update_quality.rb
+++ update_quality.rb
@@ -6,18 +6,21 @@ def update_quality(awards)
       if award.quality > 0
         if award.name != 'Blue Distinction Plus'
           award.quality -= 1
+          if award.name == 'Blue Star' and award.quality > 0
+              award.quality -=1
+          end
         end
       end
     else
       if award.quality < 50
         award.quality += 1
         if award.name == 'Blue Compare'
-          if award.expires_in < 11
+          if award.expires_in <= 10
             if award.quality < 50
               award.quality += 1
             end
           end
-          if award.expires_in < 6
+          if award.expires_in <= 5
             if award.quality < 50
               award.quality += 1
             end
@@ -34,6 +37,11 @@ def update_quality(awards)
           if award.quality > 0
             if award.name != 'Blue Distinction Plus'
               award.quality -= 1
+            end
+            if award.name == 'Blue Star'
+              if award.quality > 0
+                award.quality -=1
+              end
             end
           end
         else
