ab2d0aec61234177dce19712a132972e225ac0f2
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-10-28 23:34:45 -0400

    added comments comments, test 1 passed : Before the expiration date depreciate Blue Star award quality by 1, working on second test

--- update_quality.rb
+++ update_quality.rb
@@ -7,17 +7,26 @@ def update_quality(awards)
         if award.name != 'Blue Distinction Plus'
           award.quality -= 1
         end
-        
+
+      #  Before the expiration date depreciate Blue Star award quality by 1
+        if award.name == 'Blue Star'
+          award.quality -= 1
+        end
       end
     else
       if award.quality < 50
         award.quality += 1
         if award.name == 'Blue Compare'
+          # changed from less than 11 to less than or equal 10, because readme specified
+          # that the quality increases by 2 when there are 10 days or less left
           if award.expires_in <= 10
             if award.quality < 50
               award.quality += 1
             end
           end
+
+          # changed from less than 6 to less than or equal to 5, because the  readme specified
+          # that the quality increases by 3 when there are 10 days or less left
           if award.expires_in <= 5
             if award.quality < 50
               award.quality += 1
