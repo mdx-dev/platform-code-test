1155a1b4055e492389f9e6d81143215421c60df3
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-10-29 00:02:23 -0400

    added comments

--- update_quality.py
+++ update_quality.py
@@ -12,14 +12,19 @@ def update_quality(awards):
             if award.quality > 0:
                 if award.name != "Blue Distinction Plus":
                     award.quality -= 1
+                #  Blue Star award depreciates in quality value twice as fast as normal awards before expiration date
                 depreciate_quality(award)
         else:
             if award.quality < 50:
                 award.quality += 1
                 if award.name == "Blue Compare":
+                    # changed from less than 11 to less than or equal 10, because readme specified
+                    # that the quality increases by 2 when there are 10 days or less left
                     if award.expires_in <= 10:
                         if award.quality < 50:
                             award.quality += 1
+                    # changed from less than 11 to less than or equal 10, because readme specified
+                    # that the quality increases by 2 when there are 10 days or less left
                     if award.expires_in <= 5:
                         if award.quality < 50:
                             award.quality += 1
@@ -33,7 +38,8 @@ def update_quality(awards):
                     if award.quality > 0:
                         if award.name != "Blue Distinction Plus":
                             award.quality -= 1
-                        depreciate_quality(award)
+                            #  Blue Star award depreciates in quality value twice as fast as normal awards after expiration date
+                            depreciate_quality(award)
                 else:
                     award.quality = award.quality - award.quality
             else:
