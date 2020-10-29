a1750a56159ec0c3c4199cf8e03e6fb1681dfd00
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-10-28 23:58:13 -0400

    MVP complete on python section, 32/32 cases passed

--- update_quality.py
+++ update_quality.py
@@ -1,13 +1,22 @@
+from award import Award
+
+
+def depreciate_quality(award: Award) -> None:
+    if award.name == "Blue Star":
+        award.quality -= 1
+
+
 def update_quality(awards):
     for award in awards:
-        if award.name != 'Blue First' and award.name != 'Blue Compare':
+        if award.name != "Blue First" and award.name != "Blue Compare":
             if award.quality > 0:
-                if award.name != 'Blue Distinction Plus':
+                if award.name != "Blue Distinction Plus":
                     award.quality -= 1
+                depreciate_quality(award)
         else:
             if award.quality < 50:
                 award.quality += 1
-                if award.name == 'Blue Compare':
+                if award.name == "Blue Compare":
                     if award.expires_in < 11:
                         if award.quality < 50:
                             award.quality += 1
@@ -15,18 +24,18 @@ def update_quality(awards):
                         if award.quality < 50:
                             award.quality += 1
 
-        if award.name != 'Blue Distinction Plus':
+        if award.name != "Blue Distinction Plus":
             award.expires_in -= 1
 
         if award.expires_in < 0:
-            if award.name != 'Blue First':
-                if award.name != 'Blue Compare':
+            if award.name != "Blue First":
+                if award.name != "Blue Compare":
                     if award.quality > 0:
-                        if award.name != 'Blue Distinction Plus':
+                        if award.name != "Blue Distinction Plus":
                             award.quality -= 1
+                        depreciate_quality(award)
                 else:
                     award.quality = award.quality - award.quality
             else:
                 if award.quality < 50:
                     award.quality += 1
-
