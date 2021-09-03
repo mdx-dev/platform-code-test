43120a84e9d00c545ee8116aedf043009d294386
mguttsait <mguttsait@gmail.com>
2021-09-02 20:17:56 -0400

    add BlueFirstItem class

--- award.py
+++ award.py
@@ -10,6 +10,19 @@ class QUpdaterBase(object):
         q = (q - 1) if (exp < 0 and q > 0) else q
         return exp, q
 
+class QUpdaterBlueFirst(QUpdaterBase):
+    # Inherits QUpdaterBase Class
+    """
+    Blue First Item
+    """
+    def update(self, exp, q):
+        # First increase quality is still under 50
+        q = (q + 1) if q < 50 else q
+        exp -= 1
+        # Second increase if expiration date passed
+        q = (q + 1) if exp < 0 and q < 50 else q
+        return exp, q
+
 class Award(object):
     def __init__(self, name=None, expires_in=None, quality=None):
         self.name = name
