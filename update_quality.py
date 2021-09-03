fb9ee45a4f3aa0c03fbaa15b244d53ce94b96cce
mguttsait <mguttsait@gmail.com>
2021-09-02 20:32:23 -0400

    removing awful if logic

--- update_quality.py
+++ update_quality.py
@@ -1,32 +1,4 @@
 def update_quality(awards):
     for award in awards:
-        if award.name != 'Blue First' and award.name != 'Blue Compare':
-            if award.quality > 0:
-                if award.name != 'Blue Distinction Plus':
-                    award.quality -= 1
-        else:
-            if award.quality < 50:
-                award.quality += 1
-                if award.name == 'Blue Compare':
-                    if award.expires_in < 11:
-                        if award.quality < 50:
-                            award.quality += 1
-                    if award.expires_in < 6:
-                        if award.quality < 50:
-                            award.quality += 1
-
-        if award.name != 'Blue Distinction Plus':
-            award.expires_in -= 1
-
-        if award.expires_in < 0:
-            if award.name != 'Blue First':
-                if award.name != 'Blue Compare':
-                    if award.quality > 0:
-                        if award.name != 'Blue Distinction Plus':
-                            award.quality -= 1
-                else:
-                    award.quality = award.quality - award.quality
-            else:
-                if award.quality < 50:
-                    award.quality += 1
+        
 
