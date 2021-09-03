557af5bb0de36b37d37271bbf703940cb00353ac
mguttsait <mguttsait@gmail.com>
2021-09-02 20:33:46 -0400

    calling member update function to update each award

--- update_quality.py
+++ update_quality.py
@@ -1,4 +1,7 @@
 def update_quality(awards):
+    # Iterating Over the list of awards
     for award in awards:
+        # Updating each award by calling the member function update()
+        award.update()
         
 
