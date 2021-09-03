da2022e5b399335b1c149eab52c38cd86e163ec6
mguttsait <mguttsait@gmail.com>
2021-09-02 20:16:15 -0400

    add normal item base class

--- award.py
+++ award.py
@@ -1,3 +1,15 @@
+class QUpdaterBase(object):
+    """
+    Normal Item
+    """
+    def update(self, exp, q):
+        exp -= 1
+        # First decrease if quality is positive
+        q = (q - 1) if q > 0 else q
+        # Second decrease if expiration is passed
+        q = (q - 1) if (exp < 0 and q > 0) else q
+        return exp, q
+
 class Award(object):
     def __init__(self, name=None, expires_in=None, quality=None):
         self.name = name
