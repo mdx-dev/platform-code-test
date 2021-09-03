82068c80f277f6adf8e323bd5b9e21e2c477abf3
mguttsait <mguttsait@gmail.com>
2021-09-02 20:21:15 -0400

    add BlueCompareItem class

--- award.py
+++ award.py
@@ -23,6 +23,24 @@ class QUpdaterBlueFirst(QUpdaterBase):
         q = (q + 1) if exp < 0 and q < 50 else q
         return exp, q
 
+class QUpdaterBlueCompare(QUpdaterBlueFirst):
+    # Inherits QUpdatedBlueFirst Class
+    """
+    Blue Compare Item
+    """
+    def update(self, exp, q):
+        # First increase quality is still under 50
+        q = (q + 1) if q < 50 else q
+        # Second increase expiration days under 10
+        q = (q + 1) if (exp < 11 and q < 50) else q
+        # Third increase expiration days under 5
+        q = (q + 1) if (exp < 6 and q < 50) else q
+        exp -= 1
+        # 0 if expiration date has passed
+        q = 0 if exp < 0 else q
+        return exp, q
+
+
 class Award(object):
     def __init__(self, name=None, expires_in=None, quality=None):
         self.name = name
