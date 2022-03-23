c69b3f751adb68b72d98ce1824f7249501963832
Michael Ginalick <michael.ginalick@gmail.com>
2022-03-22 19:27:08 -0500

    Move strings into class constants

--- award.rb
+++ award.rb
@@ -3,6 +3,10 @@
 # Award = Struct.new(:name, :expires_in, :quality)
 
 class Award
+  BLUE_FIRST = 'Blue First'
+  BLUE_COMPARE = 'Blue Compare'
+  BLUE_DISTINCT_PLUS = 'Blue Distinction Plus'
+
   attr_reader :name
   attr_accessor :expires_in, :quality
 
@@ -13,20 +17,20 @@ class Award
   end
 
   def update_quality
-    if @name != 'Blue First' && name != 'Blue Compare'
-      @quality -= 1 if @quality.positive? && (@name != 'Blue Distinction Plus')
+    if @name != BLUE_FIRST && name != BLUE_COMPARE
+      @quality -= 1 if @quality.positive? && (@name != BLUE_DISTINCT_PLUS)
     elsif @quality < 50
       @quality += 1
-      if @name == 'Blue Compare'
+      if @name == BLUE_COMPARE
         @quality += 1 if @expires_in < (11) && (@quality < 50)
         @quality += 1 if @expires_in < (6) && (@quality < 50)
       end
     end
-    @expires_in -= 1 if @name != 'Blue Distinction Plus'
+    @expires_in -= 1 if @name != BLUE_DISTINCT_PLUS
     if @expires_in.negative?
-      if @name != 'Blue First'
-        if @name != 'Blue Compare'
-          @quality -= 1 if @quality.positive? && (@name != 'Blue Distinction Plus')
+      if @name != BLUE_FIRST
+        if @name != BLUE_COMPARE
+          @quality -= 1 if @quality.positive? && (@name != BLUE_DISTINCT_PLUS)
         else
           @quality -= @quality
         end
