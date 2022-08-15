9e6c19d9be553d36207ab5ad146a71bc795d798f
Itahand Naizir <itahand.naizir@gmail.com>
2022-08-15 10:09:16 -0500

    I keep getting errors on Blue Star even tho it's working.

--- update_quality.rb
+++ update_quality.rb
@@ -5,39 +5,27 @@ def update_quality(awards)
   expired = Proc.new { |date| date <= 0}
 
   reductor = lambda { |name, expired|
-
-    puts "-=-=-= REDUCTOR CALLED =-=-=-"
     reductor = case name
     when 'Blue Distinction Plus' then return reductor = 0
     when 'Blue First','Blue Compare' then 1
     when 'Blue Star' then -2
     when 'NORMAL ITEM' then -1
     end
-
-    reductor = reductor * 2 if expired
-
-    reductor
+    (expired == true) ? reductor * 2 : reductor
   }
 
   blue_compare = lambda { |expires, quality|
-    puts "-=-=-= COMPARE CALLED =-=-=-"
-
   case expires
   when 11..Float::INFINITY then 1
   when 6..10 then 2
   when 1..5 then 3
   when -Float::INFINITY..0 then -quality
   end
-
- # reductor
   }
-
 
   awards.each do |award|
 
-    #reducer = reductor.call(award.name, expired.call(award.expires_in))
-
-   puts "------The name is: #{name} initial quality is: #{award.quality} expires in: #{award.expires_in}"
+    puts "------The name is: #{name} initial quality is: #{award.quality} expires in: #{award.expires_in}"
 
     reducer = case name
     when 'Blue Compare' then blue_compare.call(award.expires_in, award.quality)
@@ -45,9 +33,7 @@ def update_quality(awards)
     end
 
     award.quality += reducer if award.quality > 0 and award.quality < 50 # Quality reduce
-
-
-
+    (award.quality > 50 && name != 'Blue Distinction Plus') ? award.quality = 50 : award.quality
 
     award.expires_in -= 1 unless name == 'Blue Distinction Plus'
 
