14c0298c9e0de678c5f6cad499a94bf93f77bcb3
Itahand Naizir <itahand.naizir@gmail.com>
2022-08-15 09:57:00 -0500

    Refactoring.

--- update_quality.rb
+++ update_quality.rb
@@ -6,6 +6,7 @@ def update_quality(awards)
 
   reductor = lambda { |name, expired|
 
+    puts "-=-=-= REDUCTOR CALLED =-=-=-"
     reductor = case name
     when 'Blue Distinction Plus' then return reductor = 0
     when 'Blue First','Blue Compare' then 1
@@ -18,28 +19,29 @@ def update_quality(awards)
     reductor
   }
 
-  blue_compare = lambda { |expires, reductor, quality|
+  blue_compare = lambda { |expires, quality|
+    puts "-=-=-= COMPARE CALLED =-=-=-"
 
   case expires
-  when expires > 10 then reductor
-  when 6..10 then reductor += 1
-  when 1..5 then reductor += 2
-  when -1000..0 then reductor = -quality
+  when 11..Float::INFINITY then 1
+  when 6..10 then 2
+  when 1..5 then 3
+  when -Float::INFINITY..0 then -quality
   end
 
-  reductor
+ # reductor
   }
 
 
   awards.each do |award|
 
-    reducer = reductor.call(award.name, expired.call(award.expires_in))
+    #reducer = reductor.call(award.name, expired.call(award.expires_in))
 
-    puts "------The name is: #{name} initial quality is: #{award.quality} the reducer: #{reducer} expires in: #{award.expires_in}"
+   puts "------The name is: #{name} initial quality is: #{award.quality} expires in: #{award.expires_in}"
 
     reducer = case name
-    when 'Blue Compare' then blue_compare.call(award.expires_in, reducer, award.quality)
-    else reducer
+    when 'Blue Compare' then blue_compare.call(award.expires_in, award.quality)
+    else reductor.call(award.name, expired.call(award.expires_in))
     end
 
     award.quality += reducer if award.quality > 0 and award.quality < 50 # Quality reduce
