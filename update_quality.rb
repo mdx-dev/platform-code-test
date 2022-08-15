0ebbd15613057f3c28bd8f7a5f90ac10e620e818
Itahand Naizir <itahand.naizir@gmail.com>
2022-08-15 08:09:03 -0500

    First draft of a solution

--- update_quality.rb
+++ update_quality.rb
@@ -1,7 +1,36 @@
 require 'award'
 
 def update_quality(awards)
+
+  expired = Proc.new { |date| date <= 0}
+  max = Proc.new { |quality| quality == 50}
+  min = Proc.new { |quality| quality == 0}
+
+  reductor = lambda { |name, expired|
+    puts name, expired
+
+    reductor = case name
+    when 'Blue Distinction Plus' then return reductor = 0
+    when 'Blue First','Blue Compare' then 1
+    when 'Blue Star' then -2
+    when 'NORMAL ITEM' then -1
+    end
+
+    reductor = reductor * 2 if expired
+
+    reductor
+  }
+
+
   awards.each do |award|
+
+
+    reducer = reductor.call(award.name, expired.call(award.expires_in))
+    puts reducer
+
+
+
+
     if award.name != 'Blue First' && award.name != 'Blue Compare'
       if award.quality > 0
         if award.name != 'Blue Distinction Plus'
