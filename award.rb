03c85ea09df33de251a8cd001642eaf1eb6fd341
Clint Philbin <clint.philbin@gmail.com>
2023-04-12 13:07:59 -0700

    Add system module and award validation

--- award.rb
+++ award.rb
@@ -1,3 +1,5 @@
+require 'award_system'
+
 class Award
   attr_accessor :name, :expires_in, :quality
 
@@ -5,5 +7,15 @@ class Award
     @name = name
     @expires_in = expires_in
     @quality = quality
+
+    validate
+  end
+
+  private
+
+  def validate
+    unless AwardSystem::AWARD_NAMES.include?(@name)
+      raise ArgumentError.new("Award name must be one of: #{AwardSystem::AWARD_NAMES}")
+    end
   end
 end
---
+++ award_system.rb
@@ -0,0 +1,3 @@
+module AwardSystem
+  AWARD_NAMES = ['NORMAL ITEM', 'Blue First', 'Blue Distinction Plus', 'Blue Compare', 'Blue Star'].freeze
+end
