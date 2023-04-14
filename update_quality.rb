a9ea54b1b2292be901785318a2d5c33985befd21
Baber Javed <baberjaved.rs@gmail.com>
2023-04-14 05:48:59 -0500

    add blue star award and refactor

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,55 @@
 require 'award'
 
+METHOD_MAP = {
+  'Blue First' => :update_blue_first_award,
+  'Blue Compare' => :update_blue_compare_award,
+  'Blue Star' => :update_blue_star_award,
+  'Blue Distinction Plus' => :update_blue_distinction_plus_award,
+}.freeze
+
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+    return if award.name == 'Blue Distinction Plus'
+
+    award.expires_in -= 1
+
+    send METHOD_MAP.fetch(award.name, :update_normal_award), award
+
+    award.quality = 0 if award.quality < 0
+    award.quality = 50 if award.quality > 50
+  end
+end
+
+def update_normal_award(award)
+  if award.expires_in > 0
+    award.quality -= 1
+  else
+    award.quality -= 2
+  end
+end
+
+def update_blue_star_award(award)
+  if award.expires_in > 0
+    award.quality -= 2
+  else
+    award.quality -= 4
+  end
+end
+
+def update_blue_first_award(award)
+  if award.expires_in > 0
+    award.quality += 1
+  else
+    award.quality += 2
+  end
+end
+
+def update_blue_compare_award(award)
+  if award.expires_in >= 0
+    award.quality += 1
+    award.quality += 1 if award.expires_in < 10
+    award.quality += 1 if award.expires_in < 5
+  else
+    award.quality = 0
   end
 end
