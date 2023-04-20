e0d2babc1e372cdb01b5d9013a3fa309b906723d
Sanchit Chopra <sanchitchopra@Sanchits-MacBook-Pro.local>
2023-04-20 13:33:19 -0700

    Added the Blue Star Award feature as required

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,29 @@
 require 'award'
 
+
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
+    case award.name
+    when 'Blue First'
       award.expires_in -= 1
+      award.quality += 1 if award.quality < 50
+      award.quality += 1 if award.expires_in < 0 && award.quality < 50
+    when 'Blue Compare'
+      award.expires_in -= 1
+      award.quality += 1 if award.quality < 50
+      award.quality += 1 if award.expires_in < 10 && award.quality < 50
+      award.quality += 1 if award.expires_in < 5 && award.quality < 50
+      award.quality = 0 if award.expires_in < 0
+    when 'Blue Star'
+      award.expires_in -= 1
+      award.quality -= 2 if award.quality > 0
+      award.quality -= 2 if award.expires_in < 0 && award.quality > 0
+    when 'Blue Distinction Plus'
+      nil # Do nothing, quality and expiration dates remain constant
+    else
+      award.expires_in -= 1
+      award.quality -= 1 if award.quality > 0
+      award.quality -= 1 if award.expires_in < 0 && award.quality > 0
     end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
-  end
+  end 
 end
