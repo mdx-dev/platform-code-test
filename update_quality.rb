fdd536cab03061b067a8bf2363ef5f71ab59255a
apple <apple@apples-MacBook-Pro.local>
2023-04-25 00:55:52 +0530

    add blue star award

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,26 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
+    case award.name
+    when 'Blue First'
+      award.quality += 1
+      award.quality += 1 if award.expires_in <= 0
+    when 'Blue Compare'
+      award.quality += 1
+      award.quality += 1 if award.expires_in <= 10
+      award.quality += 1 if award.expires_in <= 5
+      award.quality = 0 if award.expires_in <= 0
+    when 'Blue Star'
+      award.quality -= 2
+      award.quality -= 2 if award.expires_in <= 0
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+      next if award.name == 'Blue Distinction Plus'
+
+      award.quality -= 1
+      award.quality -= 1 if award.expires_in <= 0
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+    award.quality = 0 if award.quality < 0
+    award.quality = 50 if award.quality > 50 && award.name != 'Blue Distinction Plus'
+    award.expires_in -= 1 if award.name != 'Blue Distinction Plus'
   end
 end
