5f722bbf4f5157492b5e5900d1988074173fd58a
John Hurst <fuzzybunnies@gmail.com>
2023-04-24 11:41:49 -0400

    get tests working for python version

--- update_quality.py
+++ update_quality.py
@@ -1,13 +1,16 @@
 def update_quality(awards):
     for award in awards:
-        if award.name != 'Blue First' and award.name != 'Blue Compare':
+        if award.name != "Blue First" and award.name != "Blue Compare":
             if award.quality > 0:
-                if award.name != 'Blue Distinction Plus':
-                    award.quality -= 1
+                if award.name != "Blue Distinction Plus":
+                    if award.name == "Blue Star":
+                        award.quality -= 2
+                    else:
+                        award.quality -= 1
         else:
             if award.quality < 50:
                 award.quality += 1
-                if award.name == 'Blue Compare':
+                if award.name == "Blue Compare":
                     if award.expires_in < 11:
                         if award.quality < 50:
                             award.quality += 1
@@ -15,18 +18,21 @@ def update_quality(awards):
                         if award.quality < 50:
                             award.quality += 1
 
-        if award.name != 'Blue Distinction Plus':
+        if award.name != "Blue Distinction Plus":
             award.expires_in -= 1
 
         if award.expires_in < 0:
-            if award.name != 'Blue First':
-                if award.name != 'Blue Compare':
+            if award.name != "Blue First":
+                if award.name != "Blue Compare":
+                    print("award is blue star on expiration day")
                     if award.quality > 0:
-                        if award.name != 'Blue Distinction Plus':
-                            award.quality -= 1
+                        if award.name != "Blue Distinction Plus":
+                            if award.name == "Blue Star":
+                                award.quality -= 2
+                            else:
+                                award.quality -= 1
                 else:
                     award.quality = award.quality - award.quality
             else:
                 if award.quality < 50:
                     award.quality += 1
-
