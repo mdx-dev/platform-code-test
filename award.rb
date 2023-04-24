41ecf81c061471a1e607b0eba1ddaee17052aaec
John Hurst <fuzzybunnies@gmail.com>
2023-04-24 14:15:40 -0400

    updated quality prop to never set the value less than zero

--- award.rb
+++ award.rb
@@ -1,9 +1,14 @@
 class Award
-  attr_accessor :name, :expires_in, :quality
+  attr_reader :quality
+  attr_accessor :name, :expires_in
 
   def initialize(name, expires_in, quality)
     self.name = name
     self.expires_in = expires_in
     self.quality = quality
+  end
+
+  def quality=(quality)
+    @quality = quality.positive? ? quality : 0
   end
 end
