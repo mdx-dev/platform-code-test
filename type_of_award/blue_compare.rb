e803856504f4ccea8b7bfee384d576bfeea5a6cd
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-04-24 14:00:50 -0600

    adjusted return statement to catch any possible award.quality issues beyond 50

--- type_of_award/blue_compare.rb
+++ type_of_award/blue_compare.rb
@@ -3,11 +3,11 @@ class BlueCompare < AwardType
 
   def update_attributes
     award.expires_in -= 1
-    return if award.quality >= 50
     return award.quality = 0 if award.expires_in.negative?
 
     award.quality += 1
     award.quality += 1 if award.expires_in < 10
     award.quality += 1 if award.expires_in < 5
+    return award.quality = 50 if award.quality >= 50
   end
 end
