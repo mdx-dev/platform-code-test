d7737dbb10dfdc4b6e1cc27dcc7f18661f88becb
John Hurst <fuzzybunnies@gmail.com>
2023-04-24 10:32:44 -0400

    ruby - Award is now a class

--- award.rb
+++ award.rb
@@ -1,1 +1,9 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+  attr_accessor :name, :expires_in, :quality
+
+  def initialize(name, expires_in, quality)
+    self.name = name
+    self.expires_in = expires_in
+    self.quality = quality
+  end
+end
