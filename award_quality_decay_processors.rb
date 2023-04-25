aa1bb52ac11fab23f0baa8cf3f9c5ceb2ca8d017
John Hurst <fuzzybunnies@gmail.com>
2023-04-25 10:11:07 -0400

    updated processors to take an award upon initialization

--- award_quality_decay_processors.rb
+++ award_quality_decay_processors.rb
@@ -3,14 +3,22 @@ module AwardQualityDecayProcessors
     AWARD_NAME = 'Blue First'
     DECAY_AMOUNT = 1
 
-    def self.decay(award)
+    attr_accessor :award
+
+    def initialize(award)
+      return if award.nil?
+
       raise InvalidAward.new(award.name) unless award.name.eql? AWARD_NAME
 
-      award.quality += DECAY_AMOUNT
+      self.award = award
+    end
 
-      return if award.expires_in >= 0
+    def decay
+      self.award.quality += DECAY_AMOUNT
 
-      award.quality += DECAY_AMOUNT
+      return if self.award.expires_in >= 0
+
+      self.award.quality += DECAY_AMOUNT
     end
   end
 
@@ -18,21 +26,29 @@ module AwardQualityDecayProcessors
     AWARD_NAME = 'Blue Compare'
     DECAY_AMOUNT = 1
 
-    def self.decay(award)
+    attr_accessor :award
+
+    def initialize(award)
+      return if award.nil?
+
       raise InvalidAward.new(award.name) unless award.name.eql? AWARD_NAME
 
-      award.quality += DECAY_AMOUNT
+      self.award = award
+    end
+
+    def decay
+      self.award.quality += DECAY_AMOUNT
       
       # decay again
-      award.quality += DECAY_AMOUNT if award.expires_in < 10
+      self.award.quality += DECAY_AMOUNT if self.award.expires_in < 10
       
       # decay again again
-      award.quality += DECAY_AMOUNT if award.expires_in < 5
+      self.award.quality += DECAY_AMOUNT if self.award.expires_in < 5
 
-      return if award.expires_in >= 0
+      return if self.award.expires_in >= 0
 
       # set quality to zero if the award is expired
-      award.quality = 0
+      self.award.quality = 0
     end
   end
 
@@ -40,22 +56,38 @@ module AwardQualityDecayProcessors
     AWARD_NAME = 'Blue Distinction Plus'
     DECAY_AMOUNT = 0
 
-    def self.decay(award)
+    attr_accessor :award
+
+    def initialize(award)
+      return if award.nil?
+
       raise InvalidAward.new(award.name) unless award.name.eql? AWARD_NAME
-      # there's nothing to do here
-      # this doesn't change once created
+
+      self.award = award
     end
+
+    def decay; end
   end
 
   class NormalItem
     AWARD_NAME = 'NORMAL ITEM'
     DECAY_AMOUNT = 1
 
-    def self.decay(award)
-      award.quality -= DECAY_AMOUNT
-      return if award.expires_in >= 0
+    attr_accessor :award
 
-      award.quality -= DECAY_AMOUNT
+    def initialize(award)
+      return if award.nil?
+
+      raise InvalidAward.new(award.name) unless award.name.eql? AWARD_NAME
+
+      self.award = award
+    end
+
+    def decay
+      self.award.quality -= DECAY_AMOUNT
+      return if self.award.expires_in >= 0
+
+      self.award.quality -= DECAY_AMOUNT
     end
 
   end
@@ -64,11 +96,21 @@ module AwardQualityDecayProcessors
     AWARD_NAME = 'Blue Star'
     DECAY_AMOUNT = 2
 
-    def self.decay(award)
-      award.quality -= DECAY_AMOUNT
-      return if award.expires_in >= 0
+    attr_accessor :award
 
-      award.quality -= DECAY_AMOUNT
+    def initialize(award)
+      return if award.nil?
+
+      raise InvalidAward.new(award.name) unless award.name.eql? AWARD_NAME
+
+      self.award = award
+    end
+
+    def decay
+      self.award.quality -= DECAY_AMOUNT
+      return if self.award.expires_in >= 0
+
+      self.award.quality -= DECAY_AMOUNT
     end
   end
 
