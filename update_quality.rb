b9ed034b1b6fb3f4111b575c9900629b7c0641d6
Kwarteng A <kasare12@gmail.com>
2023-05-08 03:31:51 -0400

    Ruby code test for Zelis

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,66 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+    case award.name
+    when 'Blue First'
+      blue_first(award)
+      expire_decay(award)
+
+    when 'Blue Compare'
+      blue_compare(award)
+      expire_decay(award)
+    
+    when 'Blue Distinction Plus'
+      blue_dist_plus(award)
+    
+    when 'Blue Star'
+      blue_star(award)
+      expire_decay(award)
+    
+    else 
+      normal_award(award)
+      expire_decay(award)   
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+
+    check_quality_upper(award)
+    check_quality_lower(award)
   end
+end
+
+def check_quality_upper(award)
+  award.quality = 50 if award.quality > 50 and award.name != 'Blue Distinction Plus'
+end
+
+def check_quality_lower(award)
+  award.quality = 0 if award.quality < 0
+end
+
+def expire_decay(award)
+  award.expires_in -= 1
+end
+
+def normal_award(award)
+  award.quality -= 1
+  award.quality -= 1 if award.expires_in <= 0
+end
+
+def blue_first(award)
+  award.quality += 1
+  award.quality += 1 if award.expires_in <= 0
+end
+
+def blue_compare(award)
+  award.quality += 1
+  award.quality += 1 if award.expires_in < 11
+  award.quality += 1 if award.expires_in < 6
+  award.quality = award.quality - award.quality if award.expires_in <= 0
+end
+
+def blue_dist_plus(award)
+  award.quality = 80
+end
+
+def blue_star(award)
+  award.quality -= 2
+  award.quality -= 2 if award.expires_in <= 0
 end
