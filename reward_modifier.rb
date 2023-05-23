6171c03a733cb7f15de86141b7bd67befce71984
Dalvin Josias Sejour <dalvin.sejour@dutchie.com>
2023-05-23 16:53:23 -0400

    migrate to use reward modifier class

---
+++ reward_modifier.rb
@@ -0,0 +1,40 @@
+require 'award'
+
+class RewardModifier
+  attr_accessor :awards
+
+  def initialize(awards)
+    @awards = awards
+  end
+
+  def update_quality
+    white_list_for_rewards = ['Blue First', 'Blue Compare', 'Blue Distinction Plus']
+    awards.each do |award|
+      if !white_list_for_rewards.include?(award.name)
+        award.decrease_quality(1) if !award.quality.zero? && award.name != 'Blue Star'
+        award.decrease_quality(2) if !award.quality.zero? && award.name == 'Blue Star'
+      else
+        award.increase_quality(1) if award.quality < 50
+        if award.name == 'Blue Compare'
+          award.increase_quality(1) if award.expires_in < 11 && award.quality < 50
+          award.increase_quality(1) if award.expires_in < 6 && award.quality < 50
+        end
+      end
+
+      award.expires_in -= 1 if award.name != 'Blue Distinction Plus'
+
+      if award.expires_in < 0
+        if award.name != 'Blue First'
+          if award.name != 'Blue Compare'
+            award.decrease_quality(1) if award.name != 'Blue Distinction Plus' && award.quality > 0 && award.name != 'Blue Star'
+            award.decrease_quality(2) if award.name == 'Blue Star' && award.quality > 0
+          else
+            award.quality = award.quality - award.quality
+          end
+        end
+        award.increase_quality(1) if award.name == 'Blue First' && award.quality < 50
+      end
+    end
+  end
+
+end
