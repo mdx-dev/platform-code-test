7441447ec5972294c7fe6763f33d652e0f00f426
Dalvin Josias Sejour <dalvin.sejour@dutchie.com>
2023-05-23 14:43:44 -0400

    refactor to add cleaner interface

--- award.rb
+++ award.rb
@@ -1,1 +1,9 @@
-Award = Struct.new(:name, :expires_in, :quality)
+Award = Struct.new(:name, :expires_in, :quality) do |new_class|
+  def increase_quality(quality)
+    self.quality += quality
+  end
+
+  def decrease_quality(quality)
+    self.quality -= quality
+  end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -3,33 +3,27 @@ require 'award'
 def update_quality(awards)
   white_list_for_rewards = ['Blue First', 'Blue Compare', 'Blue Distinction Plus']
   awards.each do |award|
+
     if !white_list_for_rewards.include?(award.name)
-      award.quality -= 1 if award.quality > 0
+      award.decrease_quality(1) if !award.quality.zero?
     else
-      award.quality += 1 if award.quality < 50
+      award.increase_quality(1) if award.quality < 50
+
       if award.name == 'Blue Compare'
-        award.quality += 1 if award.expires_in < 11 && award.quality < 50
-        award.quality += 1 if award.expires_in < 6 && award.quality < 50
+        award.increase_quality(1) if award.expires_in < 11 && award.quality < 50
+        award.increase_quality(1) if award.expires_in < 6 && award.quality < 50
       end
     end
 
     award.expires_in -= 1 if award.name != 'Blue Distinction Plus'
 
     if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
+      if award.name != 'Blue First' && award.name != 'Blue Compare'
+        award.decrease_quality(1) if award.name != 'Blue Distinction Plus' && award.quality > 0
+      elsif award.name == 'Blue First' && award.quality < 50
+        award.increase_quality(1)
       else
-        if award.quality < 50
-          award.quality += 1
-        end
+        award.quality = 0
       end
     end
   end
