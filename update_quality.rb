7a5277cafa074c827e7d56f46c5139bc089ab7ec
Venkatnaidu22 <141445422+Venkatnaidu22@users.noreply.github.com>
2023-08-05 20:01:31 -0500

    Update update_quality.rb

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,45 @@
-require 'award'
+class Award
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
+  end
 
-def update_quality(awards)
-  awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
+  def update_quality
+    return if @name == "Blue Distinction Plus"
+
+    if @name == "Blue First"
+      @quality = [@quality + 1, 50].min
+    elsif @name == "Blue Compare"
+      if @expires_in <= 0
+        @quality = 0
+      elsif @expires_in <= 5
+        @quality = [@quality + 3, 50].min
+      elsif @expires_in <= 10
+        @quality = [@quality + 2, 50].min
+      else
+        @quality = [@quality + 1, 50].min
+      end
+    elsif @name == "Blue Star"
+      # Blue Star loses quality twice as fast as normal awards
+      if @expires_in <= 0
+        @quality = 0
+      else
+        @quality = [@quality - 2, 0].max
       end
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
+      # Normal awards lose quality twice as fast after the expiration date
+      if @expires_in <= 0
+        @quality = [@quality - 2, 0].max
       else
-        if award.quality < 50
-          award.quality += 1
-        end
+        @quality = [@quality - 1, 0].max
       end
     end
+
+    @expires_in -= 1
   end
+end
+
+def update_quality(awards)
+  awards.each(&:update_quality)
 end
